d91b0d168e315f8616b3a039dc77ad506d4e7f7e
DeviVasanth <73890267+DeviVasanth@users.noreply.github.com>
2022-07-01 15:02:17 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
+
+  function setup() {
+  video = createCapture(VIDEO);
+  video.size(550, 500);
+
+  //canvas = createCanvas(550, 550);
+  canvas.position(560,150);
+// initialize posenet model
+  
+
+  //execute poe net
+ 
+}
+
+function modelLoaded() {
+  //console
+}
+
+
+function gotPoses(results)
+{
+  if(results.length > 0)
+  {
+    console.log(results);
+
+    leftWristX = results[0].pose.leftWrist.x;
+    rightWristX = results[0].pose.rightWrist.x;
+    difference = floor(leftWristX - rightWristX);
+
+    console.log("leftWristX  = " + leftWristX  + " rightWristX = "+ rightWristX + " difference = " + difference);
+  }
+}
+
+function draw() {
+background('#6C91C2');
+
+  document.getElementById("font_size").innerHTML = "Font size of the text will be = " + difference +"px";
+textSize(difference);
+fill('#FFE787');
+//name
+text('', 50, 400);
+}
